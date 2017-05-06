60bc74845572672c088f19bd97ab4b58784dc5a9
Codeman <arms0333@algonquinlive.com>
2017-05-06 12:41:48 -0400

    navbar closes
    nabber closes

--- js/script.js
+++ js/script.js
@@ -63,6 +63,11 @@
     );
 
 
+    //close navbar once user choses
+    $(".navbar-nav li a").click(function(event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
+    });
 
     /*Preloader*/
     //<![CDATA[
