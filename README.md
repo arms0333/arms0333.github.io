4b9c77dca35224faa29cc4ef4cb8b5fe12932193
Cody Armstrong <arms0333@algonquinlive.com>
2017-12-10 09:25:54 -0500

    small commit
    just adjusted a couple small things

--- README.md
+++
@@ -1,3 +0,0 @@
-# arms0333.github.io
-
-My personal website to reach me.
--- js/script.js
+++ js/script.js
@@ -26,42 +26,6 @@
 //
 // };
 
-    //OWL CAROSEL TESTIMONIAL
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        dots:true,
-        dotsEach:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
-
-    $(document).ready(
-
-      function() {
-
-        $("html").niceScroll({
-        	cursorcolor:"#f74d65",
-        	scrollspeed :"100",
-        	cursorborder:"1px solid #f74d65",
-        	horizrailenabled: "false",
-        	cursorborderradius: "0px"
-        });
-
-      }
-
-    );
-
 
     //close navbar once user choses
     $(".navbar-nav li a").click(function(event) {
